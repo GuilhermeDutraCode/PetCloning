31aa8826f00dccf525666d8bdc9112d29aabe4e4
Guilherme Bernardes Leme Dutra <guilherme@Guilhermes-MacBook-Air.local>
2022-08-30 13:27:57 +1000

    server getting there sign in pending

--- db/migrate/20220830015122_create_users.rb
+++ db/migrate/20220830015122_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :firstName
-      t.string :lastName
-      t.email :email
+      t.text :firstName
+      t.text :lastName
+      t.text :email
       t.string :pets
-      
+
       t.timestamps
     end
   end
