264f1f8da5514018ad16ec80a4e7227f2758fd72
Adrian Greksa <adrian.greksa@gmail.com>
2022-08-31 12:49:34 +1000

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_08_30_015122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.text "firstName"
     t.text "lastName"
